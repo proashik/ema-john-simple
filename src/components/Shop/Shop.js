d4b3b64c897cdbe816a579f1c9b281fd1130314f
proashik <ashik.swfu@outlook.com>
2020-06-17 00:07:51 +0800

    ok

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,13 +8,16 @@ import Cart from '../Cart/Cart';
 
 
 const Shop = () => {
+   
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    console.log(products);
     const [cart, setCart] = useState([])
 
     const handleAddProduct = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        
     }
 
     return (
